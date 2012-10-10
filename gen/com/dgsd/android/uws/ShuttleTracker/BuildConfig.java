2bd15081dd0ce5dd6c41524adb3aff7aed0b07d3
Daniel Grech <danielgrech91@gmail.com>
2012-10-10 17:49:13 +1100

    Initial commit

---
+++ gen/com/dgsd/android/uws/ShuttleTracker/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.dgsd.android.uws.ShuttleTracker;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/dgsd/android/uws/ShuttleTracker/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dgsd.android.uws.ShuttleTracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class MainActivity extends SherlockActivity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
